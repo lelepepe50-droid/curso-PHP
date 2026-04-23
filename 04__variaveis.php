51432b4876dedf00bb5a73c027734844bc0d8ba6
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-04-22 22:17:19 -0300

    atualização do php

---
+++ 04__variaveis.php
@@ -0,0 +1,32 @@
+<?php
+$name = 'Carlos Ferreira';
+echo $name;
+echo '<br>';
+
+$name = 'João Calos';
+echo $name;
+echo '<br>';
+
+$name = 12.2;
+echo $name;
+echo '<br>';
+
+$name = 123;
+echo $name;
+echo '<br>';
+
+$name = 'linkin park';
+echo $name;
+echo '<br>';
+
+$name = 'imagine dragons';
+echo $name;
+echo '<br>';
+
+$name = 'Red Hot Chill Peppers';
+echo $name;
+echo '<br>';
+
+
+ 
+?>
---
+++ 06_array.php
@@ -0,0 +1,20 @@
+<?php
+//$carros = ['Uno','fusca','Fiesta','Clio','Corsel'];
+//echo $carros[0];
+echo '<br>';
+
+//$carro_um = ['Fiat','Palio','Branco'];
+//echo $carro_um[0];
+
+$carro_um =[
+'marca' => 'fiat',
+'modelo' =>'Palio',
+'cor' => 'Branco',
+];
+echo $carro_um['marca'];
+echo' ';
+echo $carro_um['modelo'];
+echo' ';
+echo $carro_um['cor'];
+echo ' ';
+?>
