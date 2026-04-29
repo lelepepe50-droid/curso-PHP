0e9d00b9252011606862a4b68e9b7c221fab6258
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-04-28 22:25:47 -0300

    update 11

--- 11_manipulacao_arrays.php
+++ 11_manipulacao_arrays.php
@@ -2,6 +2,7 @@
 $nomes = ['Rubens', 'Senac', 'Americana'];
 echo'<pre>';
 // strtoupper ele deixa as palavra maior
+// array_map mapeamento o array ele escuta a funtion ele retonar a funtion
 //var_dump($nomes);
 /*
 $nomes[0] = strtoupper($nomes[0]);
