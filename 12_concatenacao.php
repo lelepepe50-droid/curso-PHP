90851cfa4d41546a7e93a92d054959a4f3bcb14f
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-04 19:48:20 -0300

    Update12

---
+++ 12_concatenacao.php
@@ -0,0 +1,15 @@
+<?php
+$var1 = 'Leandro';
+$var2 = 'Andrade Pereira';
+
+//echo $var1 .' '. $var2 .' '. 20
+
+$var3 = $var1 . ' '. $var2 . ' '. 20;
+//echo $var3;
+
+$nome = 'Rubens Pereira';
+$empresa = 'Senac Americana';
+
+//echo 'O '.$nome.' é o fundador desa empresa conhecida pelo: '.$empresa;
+echo "O {.$nome.} é fundador desa empresa conhecida pelo: {$empresa}";
+?>
