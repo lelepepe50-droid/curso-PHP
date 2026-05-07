7839279bee3d679f7a89becdb2c42c2e3b675ba4
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-06 22:21:15 -0300

    updatelogin

---
+++ 01_exemplo_login/formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="validar_login.php" method="post">
+        <label>E-mail</label>
+        <input type="email" name="email"><br>
+        <label>Senha</label>
+        <input type="password" name="senha"><br>
+        <button type="submit">Acessar</button>
+   </form>
+    
+</body>
+</html>
